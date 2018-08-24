e71d04cce2e8d117b5e6a10a34f731476e592123
Olumide Francis <olumideogundele@gmail.com>
2018-08-24 03:58:39 +0100

    Update printer.js

--- js/printer.js
+++ js/printer.js
@@ -1,77 +1,153 @@
-var exec = require('cordova/exec');
+/*
+    SimpleSerial index.js
+    Created 7 May 2013
+    Modified 9 May 2013
+    by Tom Igoe
+*/
 
-var printer = {
-  platforms: ['android'],
 
-  isSupported: function() {
-    if (window.device) {
-      var platform = window.device.platform;
-      if ((platform !== undefined) && (platform !== null)) {
-        return (this.platforms.indexOf(platform.toLowerCase()) >= 0);
-      }
+var app = {
+    macAddress: "AA:BB:CC:DD:EE:FF",  // get your mac address from bluetoothSerial.list
+    chars: "",
+
+/*
+    Application constructor
+ */
+    initialize: function() {
+        this.bindEvents();
+        console.log("Starting SimpleSerial app");
+    },
+/*
+    bind any events that are required on startup to listeners:
+*/
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        connectButton.addEventListener('touchend', app.manageConnection, false);
+    },
+
+/*
+    this runs when the device is ready for user interaction:
+*/
+    onDeviceReady: function() {
+        // check to see if Bluetooth is turned on.
+        // this function is called only
+        //if isEnabled(), below, returns success:
+        var listPorts = function() {
+            // list the available BT ports:
+            bluetoothSerial.list(
+                function(results) {
+                    app.display(JSON.stringify(results));
+                },
+                function(error) {
+                    app.display(JSON.stringify(error));
+                }
+            );
+        }
+
+        // if isEnabled returns failure, this function is called:
+        var notEnabled = function() {
+            app.display("Bluetooth is not enabled.")
+        }
+
+         // check if Bluetooth is on:
+        bluetoothSerial.isEnabled(
+            listPorts,
+            notEnabled
+        );
+    },
+/*
+    Connects if not connected, and disconnects if connected:
+*/
+    manageConnection: function() {
+
+        // connect() will get called only if isConnected() (below)
+        // returns failure. In other words, if not connected, then connect:
+        var connect = function () {
+            // if not connected, do this:
+            // clear the screen and display an attempt to connect
+            app.clear();
+            app.display("Attempting to connect. " +
+                "Make sure the serial port is open on the target device.");
+            // attempt to connect:
+            bluetoothSerial.connect(
+                app.macAddress,  // device to connect to
+                app.openPort,    // start listening if you succeed
+                app.showError    // show the error if you fail
+            );
+        };
+
+        // disconnect() will get called only if isConnected() (below)
+        // returns success  In other words, if  connected, then disconnect:
+        var disconnect = function () {
+            app.display("attempting to disconnect");
+            // if connected, do this:
+            bluetoothSerial.disconnect(
+                app.closePort,     // stop listening to the port
+                app.showError      // show the error if you fail
+            );
+        };
+
+        // here's the real action of the manageConnection function:
+        bluetoothSerial.isConnected(disconnect, connect);
+    },
+/*
+    subscribes to a Bluetooth serial listener for newline
+    and changes the button:
+*/
+    openPort: function() {
+        // if you get a good Bluetooth serial connection:
+        app.display("Connected to: " + app.macAddress);
+        // change the button's name:
+        connectButton.innerHTML = "Disconnect";
+        // set up a listener to listen for newlines
+        // and display any new data that's come in since
+        // the last newline:
+        bluetoothSerial.subscribe('\n', function (data) {
+            app.clear();
+            app.display(data);
+        });
+    },
+
+/*
+    unsubscribes from any Bluetooth serial listener and changes the button:
+*/
+    closePort: function() {
+        // if you get a good Bluetooth serial connection:
+        app.display("Disconnected from: " + app.macAddress);
+        // change the button's name:
+        connectButton.innerHTML = "Connect";
+        // unsubscribe from listening:
+        bluetoothSerial.unsubscribe(
+                function (data) {
+                    app.display(data);
+                },
+                app.showError
+        );
+    },
+/*
+    appends @error to the message div:
+*/
+    showError: function(error) {
+        app.display(error);
+    },
+
+/*
+    appends @message to the message div:
+*/
+    display: function(message) {
+        var display = document.getElementById("message"), // the message div
+            lineBreak = document.createElement("br"),     // a line break
+            label = document.createTextNode(message);     // create the label
+
+        display.appendChild(lineBreak);          // add a line break
+        display.appendChild(label);              // add the message node
+    },
+/*
+    clears the message div:
+*/
+    clear: function() {
+        var display = document.getElementById("message");
+        display.innerHTML = "";
     }
-    return false;
-  },
+};      // end of app
 
-  listBluetoothDevices: function(onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'listBluetoothDevices', []);
-  },
-  connect: function(address, onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'connect', [address]);
-  },
-  disconnect: function(onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'disconnect', []);
-  },
-  flush: function(onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'flush', []);
-  },
-  printReceipt: function(charset, text, image, width, height, align, footer, linesFeed, onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'printReceipt', [charset, text, image, width, height, align, footer, linesFeed]);
-  },
-  feedPaper: function(lines, onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'feedPaper', [lines]);
-  },
-  printText: function(text, charset, onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'printText', [text, charset]);
-  },
-  printSelfTest: function (onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'printSelfTest', []);
-  },
-  getStatus: function (onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'getStatus', []);
-  },
-  getTemperature: function (onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'getTemperature', []);
-  },
-  setBarcode: function (align, small, scale, hri, height, onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'setBarcode', [align, small, scale, hri, height]);
-  },
-  printBarcode: function (type, data, onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'printBarcode', [type, data]);
-  },
-  printQRCode: function (size, eccLvl, data, onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'printQRCode', [size, eccLvl, data]);
-  },
-  printImage: function (image, width, height, align, onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'printImage', [image, width, height, align]);
-  },
-  drawPageRectangle: function (x, y, width, height, fillMode, onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'drawPageRectangle', [x, y, width, height, fillMode]);
-  },
-  drawPageFrame: function (x, y, width, height, fillMode, thickness, onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'drawPageFrame', [x, y, width, height, fillMode, thickness]);
-  },
-  selectPageMode: function (onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'selectPageMode', []);
-  },
-  selectStandardMode: function (onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'selectStandardMode', []);
-  },
-  setPageRegion: function (x, y, width, height, direction, onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'setPageRegion', [x, y, width, height, direction]);
-  },
-  printPage: function (onSuccess, onError) {
-    exec(onSuccess, onError, 'DatecsPrinter', 'printPage', []);
-  }
-};
-module.exports = printer;
